2a179fa4e0736dcfc1c916e0763ab82cb0205deb
Jonatas Passos <jpc080496@gmail.com>
2023-01-29 18:07:28 -0300

    feat: scripts de teste para Select2 mais um CT add

--- cypress/e2e/select2.cy.js
+++ cypress/e2e/select2.cy.js
@@ -1,12 +1,25 @@
 /// <reference types="cypress" />
 
 describe('Select2', () => {
-  it('deve selecionar uma única opção', () => {
-    cy.visit('/apps/select2/index.html');
+  beforeEach(() => {
+    cy.visit('/apps/select2/index.html')
+  })
 
+  it('deve selecionar uma única opção', () => {
     cy.get('.select2-selection--single')
       .click()
     cy.contains('.select2-results__option', 'Cypress')
       .click()
+  })
+
+  it('deve selecionar multiplas opções', () => {
+    const frameworks = ['Cypress', 'Robot Framework', 'Protractor']
+
+    frameworks.forEach(framework => {
+      cy.get('.select2-selection--multiple')
+        .click()
+      cy.contains('.select2-results__option', framework)
+        .click()
+    })
   })
 })
