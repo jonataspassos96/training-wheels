8f3c8ce802e6830844ed73a360dcab5089d44058
Jonatas Passos <jpc080496@gmail.com>
2023-01-31 21:55:33 -0300

    feat: scripts de teste para timeout

---
+++ cypress/e2e/timeout.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+it('exemplo de timeout', () => {
+  cy.visit('/timeout')
+
+  cy.contains('button', 'Habilita').click()
+
+  // cy.wait(7000)
+
+  cy.get('#firstname')
+    .should('be.visible')
+    .type('Fernando', { timeout: 7000 })
+})
