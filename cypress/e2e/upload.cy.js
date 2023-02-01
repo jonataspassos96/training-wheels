ade9cdf55d9ff1f79047502f5710065b5d1e6e06
Jonatas Passos <jpc080496@gmail.com>
2023-02-01 09:06:07 -0300

    feat: scripts de teste para upload

---
+++ cypress/e2e/upload.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+it('deve fazer o upload da minha foto', () => {
+  cy.visit('/upload')
+
+  const imageFile = 'cypress/fixtures/minion.jpg'
+
+  cy.get('input[name=file]').selectFile(imageFile, { force: true })
+
+  cy.get('input[value=Upload]').click()
+
+  cy.get('img[src="/uploads/minion.jpg"]', { timeout: 7000 })
+    .should('be.visible')
+})
